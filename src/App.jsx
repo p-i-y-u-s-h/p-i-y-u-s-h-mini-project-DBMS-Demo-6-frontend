46e95a42d2af949527eb0bd04943e7253a64c3bb
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-26 00:27:12 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import Navbar from "./components/Navbar";
+
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes , Route } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+
 const App = ()=>{
+  const {authUser}=useAuthStore()
   return (
     <div >
+
       <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/signup" element={<SignupPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/settings" element={<SettingsPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+      </Routes>
+
     </div>
   )
 }
---
+++ src/store/useAuthStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+export const useAuthStore = create((set)=>({
+    authUser: null,
+    isSigninUp: false,
+    isLoggingIng: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+}))
