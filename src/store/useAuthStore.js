6d0912e2a77add5bedec7e94af06f321e7df80e3
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-27 01:58:27 +0530

    .

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
 
 export const useAuthStore = create((set)=>({
     authUser: null,
@@ -7,4 +8,20 @@ export const useAuthStore = create((set)=>({
     isUpdatingProfile: false,
 
     isCheckingAuth: true,
+
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+            set({authUser:res.data});
+        } catch (error) {
+            console.log("Error in checkAuth : ",error);
+            set({authUser:null});
+        } finally{
+            set({isCheckingAuth:false});
+        }
+    },
+
+    signup : async (data) => {
+        
+    }
 }))
