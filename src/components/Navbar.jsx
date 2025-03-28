a7b335adb1eedff94b56f7e0a0c3535228f5f1c1
Piyush Mahajan <mahajanpiyush005@gmail.com>
2025-03-29 00:54:58 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,47 @@
+import { Link } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
-    return <div>Navbar</div>;
+  const { logout, authUser } = useAuthStore();
+
+  return (
+    <header className="fixed top-0 w-full z-40 border-b border-base-300 bg-base-100/80 backdrop-blur-lg">
+      <div className="container mx-auto px-6 h-16">
+        <div className="flex items-center justify-between h-full">
+          <Link to="/" className="flex items-center gap-3 hover:opacity-80 transition-all">
+            <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+              <MessageSquare className="w-5 h-5 text-primary" />
+            </div>
+            <h1 className="text-xl font-semibold tracking-wide">Chat-App</h1>
+          </Link>
+
+          <div className="flex items-center gap-3">
+            <Link to="/settings" className="btn btn-sm gap-2 transition-colors">
+              <Settings className="w-4 h-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+
+            {authUser && (
+              <>
+                <Link to="/profile" className="btn btn-sm gap-2">
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button
+                  className="btn btn-sm gap-2 text-red-600 hover:bg-red-100 transition-all"
+                  onClick={logout}
+                >
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 export default Navbar;
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -40,8 +40,16 @@ export const useAuthStore = create((set)=>({
         } finally {
             set({ isSigningUp: false });
         }
+    },
+    
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set ({ authUser : null});
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
     }
-    
-    
     
 }))
